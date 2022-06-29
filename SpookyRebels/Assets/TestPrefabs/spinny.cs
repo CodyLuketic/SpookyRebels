91be662e47270bd3f2c8c21fa23bada56f2d7879
Cody Luketic <luketica@miamioh.edu>
2022-06-29 16:47:10 -0400

    Testing Script Update

--- SpookyRebels/Assets/TestPrefabs/spinny.cs
+++ SpookyRebels/Assets/TestPrefabs/spinny.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class spinny : MonoBehaviour
 {
+    private int speed = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class spinny : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(0, 1, 0);
+        transform.position = transform.position + new Vector3(speed, 5, 0);
     }
 }
