c55e9399948e4eaad2b0e63712da54246f193849
Cody Luketic <luketica@miamioh.edu>
2022-08-28 19:19:12 -0400

    EnemyAI1

---
+++ SpookyRebels/Assets/Cody/EnemyFollow.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollow : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent enemy = null;
+
+    [SerializeField]
+    private Transform player = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Follow()
+    {
+        //enemy.SetDestination;
+    }
+}
