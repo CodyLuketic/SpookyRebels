b6a9e8cca8cb0e3258deeac719fa896c9f063e68
humanpersonssb <90877716+humanpersonssb@users.noreply.github.com>
2022-09-25 12:24:19 -0700

    Lucy Gun Fixes and Crystal Crab Started
    fixed gun issues and started working on Crystal Crab

--- SpookyRebels/Assets/Ethan/Scripts/Skill.cs
+++ SpookyRebels/Assets/Ethan/Scripts/Skill.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skill : MonoBehaviour
+[System.Serializable]
+public class Skill
 {
     public string skillName;
     public Sprite skillIcon;
