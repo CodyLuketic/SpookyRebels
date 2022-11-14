3bb41aed3382e65996643eea36e593adfe165ad1
humanpersonssb <90877716+humanpersonssb@users.noreply.github.com>
2022-11-13 23:10:24 -0800

    Typo
    Remembered a typo i needed to change in a description

--- SpookyRebels/Assets/Ethan/No Boys Allowed ;)/BurnBook.cs
+++ SpookyRebels/Assets/Ethan/No Boys Allowed ;)/BurnBook.cs
@@ -24,4 +24,5 @@ public class BurnBook : MonoBehaviour
     // Entry 15: Me and Haley were fire in Codenames. Hannah wishes she could be this good frfr
     // Entry 16: Hannah, Madison, and me are going to Red Robin today! Hopefully Hannah will behave for once but you know her frfr
     // Entry 17: Red Robin was fun even though Hannah was working her way through all the men. Hannah be Hannah frfr
+    // Entry 18: OMG, Hannah missed when my gossip about Luke killed half the town. The town wasn't happy frfr
 }
